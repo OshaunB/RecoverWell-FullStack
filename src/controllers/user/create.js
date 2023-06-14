cc8e7b168689782d1cea8ee9af37595acff57be8
ChukwukaEJinkonye-jpg <chukwukaejinkonye@gmail.com>
2023-06-14 15:59:23 -0400

    user and post model set up

--- src/controllers/user/create.js
+++ src/controllers/user/create.js
@@ -2,11 +2,11 @@ const createUser = async (req, res) => {
   const {
     session,
     db: { User },
-    body: { username, password },
+    body: { username, password,  email, gender, DOB, avatar, full_name},
   } = req;
 
   // TODO: check if username is taken, what should you return?
-  const user = await User.create(username, password);
+  const user = await User.create(username, password, email, gender, DOB, full_name, avatar);
   session.userId = user.id;
 
   res.send(user);
