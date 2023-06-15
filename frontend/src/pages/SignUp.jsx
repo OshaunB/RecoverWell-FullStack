e530a09bf8d038fa866686940d26700101b899b4
sodiqlab <henryymustapha@gmail.com>
2023-06-15 11:32:29 -0400

    revisions plus some additional images

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -40,7 +40,6 @@ export default function SignUpPage() {
 
   if (currentUser) return <Navigate to="/" />;
 
-<<<<<<< HEAD
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -60,131 +59,6 @@ export default function SignUpPage() {
                 type="text"
                 name="fullname"
                 placeholder="John Doe"
-=======
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setErrorText('');
-    if (!username || !password) return setErrorText('Missing username or password');
-
-    const [user, error] = await createUser({ username, password });
-    if (error) return setErrorText(error.statusText);
-
-    setCurrentUser(user);
-    navigate('/');
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    if (name === 'username') setUsername(value);
-    if (name === 'password') setPassword(value);
-  };
-//full name , dob , gender, avatar
-//stretch feature - looking to add a feature to limit url inputs length
-
-/*
-<div>
-            <label htmlFor="url" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your profile Url</label>
-            <input
-              type="url"
-              name="url"
-              id="url"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Profile image URL"
-              required
-            />
-          </div>
-
-
-*/
-
-  return <>
-   <section className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-      <img className="w-8 h-8 mr-2" src="assets/RecoverWell Dummy Logo.PNG" alt="logo" />
-      Recover Well
-    </a>
-    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Create an account
-        </h1>
-        <form className="space-y-4 md:space-y-6" action="#">
-          <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
-            <input
-              type="fullname"
-              name="fullname"
-              id="fullname"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="John Doe"
-              required
-            />
-          </div>
-          
-          <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@company.com"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Username</label>
-            <input
-              type="username"
-              name="username"
-              id="username"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Desired Username"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="Gender" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
-            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-              <option selected>Select your Gender</option>
-              <option value="Man">Man</option>
-              <option value="Woman">Woman</option>
-              <option value="Other">Other</option>
-              <option value="NA">Prefer Not To Say</option>
-            </select>
-
-          </div>
-          <div>
-  <label htmlFor="dob" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date of Birth</label>
-  <input
-    type="date"
-    name="dob"
-    id="dob"
-    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    required
-  />
-</div>
-
-          <div className="flex items-start">
-            <div className="flex items-center h-5">
-              <input
-                id="terms"
-                aria-describedby="terms"
-                type="checkbox"
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
->>>>>>> 80a5b36 (log in page cleaned up, now needs functionality.)
                 required
               />
 
