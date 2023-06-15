713c421d42627e5e2ec475b6f9e8f914436ddc32
Nayanp35 <prajapatinayan35@gmail.com>
2023-06-15 10:17:33 -0400

    sign up works and created cards for discusssion page

--- frontend/src/components/SiteHeadingAndNav.jsx
+++ frontend/src/components/SiteHeadingAndNav.jsx
@@ -1,18 +1,27 @@
-import { NavLink } from "react-router-dom";
-import { useContext, useState } from "react";
+"use client";
+
+import { Dropdown, Navbar, Avatar } from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
 import CurrentUserContext from "../contexts/current-user-context";
-'use client';
-import { Dropdown, Navbar, Avatar } from 'flowbite-react';
 
 export default function SiteHeadingAndNav() {
-  const { currentUser } = useContext(CurrentUserContext);
+  const navigate = useNavigate();
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+
+  const handleSignOut = async () => {
+    const response = await fetch("/api/logout", {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      setCurrentUser(null);
+      navigate("/");
+    }
+  };
 
   return (
-    <Navbar
-      fluid
-      rounded
-    >
-      <Navbar.Brand href="https://flowbite-react.com">
+    <Navbar fluid rounded>
+      <Navbar.Brand to="/">
         <img
           alt="RecoverWell Dummy Logo"
           className="mr-3 h-6 sm:h-9"
@@ -22,68 +31,51 @@ export default function SiteHeadingAndNav() {
           RecoverWell
         </span>
       </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Dropdown
-          inline
-          label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded/>}
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">
-              Bonnie Green
-            </span>
-            <span className="block truncate text-sm font-medium">
-              name@flowbite.com
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Item>
-            Dashboard
-          </Dropdown.Item>
-          <Dropdown.Item>
-            Settings
-          </Dropdown.Item>
-          <Dropdown.Item>
-            Earnings
-          </Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>
-            Sign out
-          </Dropdown.Item>
-        </Dropdown>
-        <Navbar.Toggle />
-      </div>
+      <>
+        {currentUser && (
+          <div className="flex md:order-2">
+            <Dropdown
+              inline
+              label={
+                <Avatar
+                  alt="User settings"
+                  img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                  rounded
+                />
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">{currentUser?.full_name}</span>
+                <span className="block truncate text-sm font-medium">
+                  {currentUser?.email}
+                </span>
+              </Dropdown.Header>
+              <Dropdown.Item>Dashboard</Dropdown.Item>
+              <Dropdown.Item>Settings</Dropdown.Item>
+              <Dropdown.Item>Earnings</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item onClick={handleSignOut}>Sign out</Dropdown.Item>
+            </Dropdown>
+            <Navbar.Toggle />
+          </div>
+        )}
+      </>
       <Navbar.Collapse>
-        <Navbar.Link
-          active
-          href="/"
-        >
-          <p>
-            Home
-          </p>
-        </Navbar.Link>
-        <Navbar.Link href="#">
-          Events
-        </Navbar.Link>
-        <Navbar.Link href="/discussions">
-          Discussions
-        </Navbar.Link>
-         {currentUser ?
-        <Navbar.Link href={`/users/${currentUser.id}`}>{currentUser.username}
-          Login In
-        </Navbar.Link>
-        :
-        <>
-        <Navbar.Link href="/login">
-          Login
-        </Navbar.Link>
-        <Navbar.Link href="/sign-up">
-          Sign Up
-        </Navbar.Link>
-        </>
-}
-        <Navbar.Link href="/users">
-          Users
-        </Navbar.Link>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <Link to="#">Events</Link>
+        <Link to="/discussions">Discussions</Link>
+        <Link to="/users">Users</Link>
+        {currentUser ? (
+          <Link to={`/users/${currentUser.id}`}>{currentUser.username}</Link>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/sign-up">Sign Up</Link>
+          </>
+        )}
       </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
