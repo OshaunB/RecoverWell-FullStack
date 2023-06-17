47a3f7cb2c09636807eb727a8f82ecfd4cb36eef
oshaunberridge <oshaun1250@gmail.com>
2023-06-16 22:18:48 -0400

    broke login page into react components

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,126 +1,13 @@
-import { useContext } from "react";
-import { useNavigate, Navigate, Link } from "react-router-dom";
-import { logUserIn } from "../adapters/auth-adapter";
-import CurrentUserContext from "../contexts/current-user-context";
+import LoginImage from "../components/LoginImage.jsx";
+import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
-  const navigate = useNavigate();
-  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-
-  const handleLoginSubmit = async (e) => {
-    e.preventDefault();
-    const userData = {
-      username: e.target.username.value,
-      password: e.target.password.value,
-    };
-    const [user, error] = await logUserIn(userData);
-    if (error) return console.log(error);
-    setCurrentUser(user);
-    navigate(`/`);
-  };
-
-  if (currentUser) return <Navigate to="/" />;
-
   return (
     <>
       <div className="flex items-center justify-center w-full h-full mt-40">
         <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
-          <div
-            className="hidden bg-cover lg:block lg:w-1/2"
-            style={{
-              backgroundImage:
-                "url('assets/coz_a_beautiful_peaceful_image_of_a_buddhist_monk_meditating_su_7d51d875-94dd-4dcd-b0a5-bee60b91822d.png')",
-            }}
-          ></div>
-
-          <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
-            <div className="flex justify-center mx-auto">
-              <img
-                className="w-auto h-7 sm:h-8"
-                src="assets/RecoverWell Dummy Logo.PNG"
-                alt=""
-              />
-            </div>
-
-            <p className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">
-              Welcome back!
-            </p>
-
-            <Link
-              to="#"
-              className="flex items-center justify-center mt-4 text-white transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 bg-gray-800"
-            >
-              <div className="px-4 py-2">
-                <img
-                  className="w-auto h-7 sm:h-8"
-                  src="assets/RecoverWell Dummy Logo.PNG"
-                  alt=""
-                />
-              </div>
-              YOUR JOURNEY, OUR MISSION
-            </Link>
-
-            <div className="flex items-center justify-between mt-4">
-              <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
-              <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
-            </div>
-            <form onSubmit={handleLoginSubmit}>
-              <div className="mt-4">
-                <label
-                  className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-                  htmlFor="LoggingEmailAddress"
-                >
-                  UserName
-                </label>
-                <input
-                  id="username"
-                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-                  type="text"
-                />
-              </div>
-
-              <div className="mt-4">
-                <div className="flex justify-between">
-                  <label
-                    className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-                    htmlFor="loggingPassword"
-                  >
-                    Password
-                  </label>
-                </div>
-                <input
-                  id="password"
-                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-                  type="password"
-                />
-              </div>
-              <Link
-                to="#"
-                className="text-xs text-gray-500 dark:text-gray-300 hover:underline"
-              >
-                Forget Password?
-              </Link>
-
-              <div className="mt-6">
-                <button
-                  type="submit"
-                  className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
-                >
-                  Sign In
-                </button>
-              </div>
-            </form>
-            <div className="flex items-center justify-between mt-4">
-              <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-              <Link
-                to="/sign-up"
-                className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
-              >
-                or sign up
-              </Link>
-              <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-            </div>
-          </div>
+          <LoginImage />
+          <LoginForm />
         </div>
       </div>
     </>
