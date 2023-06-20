654e7f99a14ec4a4c7280a71a95fd2bff649291e
sodiqlab <henryymustapha@gmail.com>
2023-06-20 11:30:28 -0400

    changes to landing page as well as structure. added some componenets

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+
+const API_KEYS = {
+    UPLOADCARE_API_KEY: "YO3fe0a0dbda27475b44ac",
+  };
+  export default API_KEYS;
---
+++ frontend/src/landingpagecomponents/Main.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
+
+export default function Main() {
+  return (
+    <section className="text-black body-font lg:pt-20">
+      <div className="container px-5 pt-32 mx-auto lg:px-4 lg:py-4">
+        <div className="flex flex-col w-full mb-2 text-left md:text-center">
+          <h1 className="mb-2 text-6xl font-bold tracking-tighter text-white lg:text-8xl md:text-7xl">
+            <span>Let us Join you </span>
+            <br className="hidden lg:block" />
+            On Your Journey
+          </h1>
+          <br />
+          <p className="mx-auto text-xl font-normal leading-relaxed text-gray-600 dark:text-gray-300 lg:w-2/3">
+            As we Recover , it is our duty to help ourseleves{" "}
+          
+            in all ways that we can 
+          </p>
+        </div>
+      </div>
+      <div className="container flex flex-col items-center justify-center py-8 mx-auto rounded-lg md:p-1 p-3">
+        <img
+          className="object-cover object-center w-full mb-10 border-gray-200 dark:border-gray-900 g327 border rounded-lg shadow-md"
+          alt="hero"
+          src="../assets/Light Blub with recover text - Black Background (1).jpg"
+        />
+      </div>
+      <section className="text-gray-600 body-font">
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-10 mx-auto">
+            <div className="flex flex-wrap -m-4 text-center">
+              <div className="p-4 sm:w-1/3 w-1/2">
+                <h2 className="title-font font-medium sm:text-5xl text-3xl text-white">
+                  <CountUp end={940} redraw={true}>
+                    {({ countUpRef, start }) => (
+                      <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                      </VisibilitySensor>
+                    )}
+                  </CountUp>
+                </h2>
+                <p className="leading-relaxed">Users</p>
+              </div>
+              <div className="p-4 sm:w-1/3 w-1/2">
+                <h2 className="title-font font-medium sm:text-5xl text-3xl text-white">
+                  <CountUp end={740} redraw={true}>
+                    {({ countUpRef, start }) => (
+                      <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                      </VisibilitySensor>
+                    )}
+                  </CountUp>
+                </h2>
+                <p className="leading-relaxed">Subscribes</p>
+              </div>
+              <div className="p-4 sm:w-1/3 w-1/2">
+                <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">
+                  <CountUp end={315} redraw={true}>
+                    {({ countUpRef, start }) => (
+                      <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                      </VisibilitySensor>
+                    )}
+                  </CountUp>
+                </h2>
+                <p className="leading-relaxed">Members</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-10 mx-auto">
+            <div className="flex flex-wrap -m-4 text-center">
+              <div className="p-4 sm:w-1/3 w-1/2">
+                <h2 className="title-font font-medium sm:text-5xl text-3xl text-white">
+                  <CountUp end={940} redraw={true}>
+                    {({ countUpRef, start }) => (
+                      <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                      </VisibilitySensor>
+                    )}
+                  </CountUp>
+                </h2>
+                <p className="leading-relaxed">Users</p>
+              </div>
+              <div className="p-4 sm:w-1/3 w-1/2">
+                <h2 className="title-font font-medium sm:text-5xl text-3xl text-white">
+                  <CountUp end={740} redraw={true}>
+                    {({ countUpRef, start }) => (
+                      <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                      </VisibilitySensor>
+                    )}
+                  </CountUp>
+                </h2>
+                <p className="leading-relaxed">Community</p>
+              </div>
+              <div className="p-4 sm:w-1/3 w-1/2">
+                <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">
+                  <CountUp end={315} redraw={true}>
+                    {({ countUpRef, start }) => (
+                      <VisibilitySensor onChange={start} delayedCall>
+                        <span ref={countUpRef} />
+                      </VisibilitySensor>
+                    )}
+                  </CountUp>
+                </h2>
+                <p className="leading-relaxed">Downloads</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+    </section>
+  );
+}
+
