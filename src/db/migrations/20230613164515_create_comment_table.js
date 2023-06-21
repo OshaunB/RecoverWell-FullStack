f68f2fe93e54a8a7d094adb2ffe28f8667b57012
Nayan Prajapati <111540429+Nayanp35@users.noreply.github.com>
2023-06-21 15:17:25 -0400

    Update 20230613164515_create_comment_table.js

--- src/db/migrations/20230613164515_create_comment_table.js
+++ src/db/migrations/20230613164515_create_comment_table.js
@@ -6,7 +6,6 @@ exports.up = (knex) => knex.schema.createTable("comments", (table) => {
   table.increments("id").primary();
   table.integer("user_id").notNullable();
   table.integer("post_id").notNullable();
-  table.integer("number_of_likes").defaultTo(0);
   table.string("comment").notNullable();
   table.timestamps(true, true);
 });
