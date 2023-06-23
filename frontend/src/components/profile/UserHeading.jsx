4650b7a27350388e59087549a3983a341d4f4157
oshaunberridge <oshaun1250@gmail.com>
2023-06-23 14:52:59 -0400

    userEvents component are now rendering

--- frontend/src/components/profile/UserHeading.jsx
+++ frontend/src/components/profile/UserHeading.jsx
@@ -4,71 +4,58 @@ import { useNavigate, useParams } from "react-router-dom";
 import { logUserOut } from "../../adapters/auth-adapter";
 import { fetchHandler } from "../../utils";
 
-
-export default function UserHeading() {
-    const navigate = useNavigate();
-    const { currentUser, setCurrentUser } = useContext(CurrentUserContext)
-    const { id } = useParams();
-    const isCurrentUserProfile = currentUser && currentUser.id === Number(id);
-
-
-    console.log(currentUser)
-
-    const handleLogout = async () => {
-        logUserOut();
-        setCurrentUser(null);
-        navigate('/');
-    };
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const [data, error] = await fetchHandler(`/api/posts`);
-    //         if (error) return console.log(error)
-    //         console.log(data)
-
-    //         data.forEach((post) => {
-    //             if (post.user_id === currentUser.id) {
-    //                 console.log('hi', post)
-    //             }
-    //         })
-    //     }
-
-    //     fetchData()
+import {
+    Dialog,
+    DialogBody,
+} from "@material-tailwind/react";
+import { useState } from "react";
 
 
-    //     // let arrayData = fetchData()
-    //     // arrayData.forEach((post) => {
-    //     //     if(post.user_id === currentUser.id){
-    //     //         console.log('1')
-    //     //     }
-    //     // })
-    //     // console.log(arrayData)
+
+export default function UserHeading(props) {
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen((cur) => !cur);
 
 
-    // })
-    // https://images.unsplash.com/photo-1489205228477-131202ff3403?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2064&q=80
-    // className="bg-gray-900"
     return (
-        <div className="bg-gray-900">
+        <div className="flex justify-center align-center">
             <div className="flex w-full">
-                <div className="flex align-center justify-center bg-red-200 ml-8 ">
-                    <img className="w-5/6 my-8 mx-12 bg-orange-200 shadow-2xl" src={currentUser.avatar} alt="" />
+
+                <div
+                    className="flex align-center justify-center ml-8 cursor-pointer transition-opacity hover:opacity-90"
+                    onClick={handleOpen}>
+                    <img className="w-4/6 my-8 mx-12 bg-orange-200 shadow-2xl rounded-full" src={props.avatar} alt="User Profile Pic" />
                 </div>
+
+                <Dialog size="md" open={open} handler={handleOpen}>
+                    <DialogBody divider={true} className="p-0">
+                        <img
+                            alt="nature"
+                            className="h-[48rem] w-full object-cover object-center"
+                            src={props.avatar}
+                        />
+                    </DialogBody>
+                </Dialog>
+
+
+
+{/* 
                 <div className="flex w-full mx-8 items-center ">
                     <div className="w-1/2">
-                        <h1 className="m-auto w-full text-3xl font-medium text-white py-2">
-                            {"Username: " + currentUser.username}
+                        <h1 className="m-auto w-full text-3xl font-medium text-gray py-2">
+                            {"Username: " + props.username}
                         </h1>
-                        <h1 className="m-auto w-full text-3xl font-medium text-white py-2">
-                            {"Gender: " + currentUser.gender}
+                        <h1 className="m-auto w-full text-3xl font-medium text-gray py-2">
+                            {"Gender: " + props.gender}
                         </h1>
-                        {!!isCurrentUserProfile && <button className="text-3xl font-medium text-orange-400 py-2" onClick={handleLogout}>Log Out</button>}
                     </div>
                     <div className="w-1/2">
 
                     </div>
 
-                </div>
+                </div> */}
+
             </div>
         </div>
 
--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,45 +1,118 @@
-import CommentIcon from "@mui/icons-material/Comment";
-import ShareIcon from "@mui/icons-material/Share";
-import { Card, Typography } from "@material-tailwind/react";
 
-export default function RenderPosts(props) {
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Tooltip,
+} from "@material-tailwind/react";
+ 
+export default function Example() {
   return (
-    <div className="flex justify-center py-3">
-      <Card className="w-full sm:w-2/3 md:w-1/2 lg:w-1/2 bg-white shadow-lg rounded-lg p-4 hover:bg-gray-100">
-        <div className="flex items-center mb-4">
-          <img
-            src={props.avatar || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"}
-            alt="Profile Picture"
-            className="w-8 h-8 sm:w-10 sm:h-10 rounded-full mr-4"
-          />
-          <div>
-            <Typography variant="h6" className="font-bold text-sm sm:text-base">
-              @{props.username}
-            </Typography>
-            <Typography color="textSecondary" variant="body2" className="text-xs sm:text-sm">
-              {props.time}
-            </Typography>
-          </div>
-        </div>
-        <Typography className="text-gray-800 text-xs sm:text-sm mb-4 pl-5">{props.content}</Typography>
-        <div className="flex justify-around">
-          <div>
-            <span onClick={() => props.clickLike(props.post)}>
-              {props.icon}
-            </span>{" "}
-            {props.likes}
-          </div>
-          <div
-            className="cursor-pointer"
-            onClick={() => props.clickComment(props.post)}
+    <Card className="w-96">
+      <CardHeader floated={false} className="h-80">
+        <img src="/img/team-3.jpg" alt="profile-picture" />
+      </CardHeader>
+      <CardBody className="text-center">
+        <Typography variant="h4" color="blue-gray" className="mb-2">
+          Natalie Paisley
+        </Typography>
+        <Typography color="blue" className="font-medium" textGradient>
+          CEO / Co-Founder
+        </Typography>
+      </CardBody>
+      <CardFooter className="flex justify-center gap-7 pt-2">
+        <Tooltip content="Like">
+          <Typography
+            as="a"
+            href="#facebook"
+            variant="lead"
+            color="blue"
+            textGradient
           >
-            <CommentIcon fontSize="small" /> Comment
-          </div>
-          <div>
-            <ShareIcon fontSize="small" /> Share
-          </div>
-        </div>
-      </Card>
-    </div>
+            <i className="fab fa-facebook" />
+          </Typography>
+        </Tooltip>
+        <Tooltip content="Follow">
+          <Typography
+            as="a"
+            href="#twitter"
+            variant="lead"
+            color="light-blue"
+            textGradient
+          >
+            <i className="fab fa-twitter" />
+          </Typography>
+        </Tooltip>
+        <Tooltip content="Follow">
+          <Typography
+            as="a"
+            href="#instagram"
+            variant="lead"
+            color="purple"
+            textGradient
+          >
+            <i className="fab fa-instagram" />
+          </Typography>
+        </Tooltip>
+      </CardFooter>
+    </Card>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+// import CommentIcon from "@mui/icons-material/Comment";
+// import ShareIcon from "@mui/icons-material/Share";
+// import { Card, Typography } from "@material-tailwind/react";
+
+// export default function RenderPosts(props) {
+//   return (
+//     <div className="flex justify-center py-3">
+//       <Card className="w-full sm:w-2/3 md:w-1/2 lg:w-1/2 bg-white shadow-lg rounded-lg p-4 hover:bg-gray-100">
+//         <div className="flex items-center mb-4">
+//           <img
+//             src={props.avatar || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"}
+//             alt="Profile Picture"
+//             className="w-8 h-8 sm:w-10 sm:h-10 rounded-full mr-4"
+//           />
+//           <div>
+//             <Typography variant="h6" className="font-bold text-sm sm:text-base">
+//               @{props.username}
+//             </Typography>
+//             <Typography color="textSecondary" variant="body2" className="text-xs sm:text-sm">
+//               {props.time}
+//             </Typography>
+//           </div>
+//         </div>
+//         <Typography className="text-gray-800 text-xs sm:text-sm mb-4 pl-5">{props.content}</Typography>
+//         <div className="flex justify-around">
+//           <div>
+//             <span onClick={() => props.clickLike(props.post)}>
+//               {props.icon}
+//             </span>{" "}
+//             {props.likes}
+//           </div>
+//           <div
+//             className="cursor-pointer"
+//             onClick={() => props.clickComment(props.post)}
+//           >
+//             <CommentIcon fontSize="small" /> Comment
+//           </div>
+//           <div>
+//             <ShareIcon fontSize="small" /> Share
+//           </div>
+//         </div>
+//       </Card>
+//     </div>
+//   );
+// }
