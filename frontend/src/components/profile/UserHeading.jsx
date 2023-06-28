5b2adad6d85814154e1c44aa8e22fc08aeee13c7
O'Shaun Berridge <114107931+OshaunB@users.noreply.github.com>
2023-06-28 17:08:34 -0400

    Update UserHeading.jsx

--- frontend/src/components/profile/UserHeading.jsx
+++ frontend/src/components/profile/UserHeading.jsx
@@ -5,17 +5,24 @@ import {
   Typography,
   Dialog,
   DialogBody,
+  Textarea,
+  Button,
 } from "@material-tailwind/react";
+
+import { fetchHandler, getPatchOptions } from "../../utils";
+
 
 export default function UserHeading(props) {
   const [open, setOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  
   const handleOpen = () => setOpen((cur) => !cur);
 
   function calculateAge(dateString) {
     var currentDate = new Date();
     var birthDate = new Date(dateString);
     var age = currentDate.getUTCFullYear() - birthDate.getUTCFullYear();
-  
+
     // Check if the current date has passed the birthdate in the current year
     if (
       currentDate.getUTCMonth() < birthDate.getUTCMonth() ||
@@ -24,10 +31,21 @@ export default function UserHeading(props) {
     ) {
       age--; // Subtract 1 from the age if the birthday hasn't occurred yet
     }
-  
+
     return age;
   }
-  
+
+ 
+
+  const handleQuoteUpdate = async (e) => {
+    e.preventDefault();
+    const quote = e.target.quote.value;
+    const [data, error] = await fetchHandler(`/api/users/quote/${props.id}`, getPatchOptions({ favoriteQuote: quote }));
+    if (error) return console.log(error);
+    setIsEditing(false);
+    props.setFavoriteQuote(data.favorite_quote)
+  };
+
   return (
     <div className="flex justify-center mb-6 mt-2">
       <div className="flex w-full">
@@ -52,6 +70,49 @@ export default function UserHeading(props) {
             <Typography color="blue" className="font-medium" textGradient>
               {calculateAge(props.age)}
             </Typography>
+            {isEditing ? (
+              <form onSubmit={handleQuoteUpdate}>
+                <Textarea
+                  variant="static"
+                  placeholder="Favorite Quote"
+                  rows={2}
+                  id="quote"
+                />
+                <div className="w-full flex justify-between py-1.5">
+                  <div className="flex gap-2">
+                    <Button
+                      size="sm"
+                      color="red"
+                      variant="text"
+                      rounded={true}
+                      onClick={() => setIsEditing(false)}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                    type="submit"
+                      size="sm"
+                      rounded={true}
+                    >
+                      Save
+                    </Button>
+                  </div>
+                </div>
+              </form>
+            ) : (
+              <Typography color="blue" className="font-medium" textGradient>
+                "{props.favoriteQuote}"
+                <Button
+                  size="sm"
+                  color="blue"
+                  variant="text"
+                  rounded={true}
+                  onClick={() => setIsEditing(true)}
+                >
+                  Edit
+                </Button>
+              </Typography>
+            )}
           </CardBody>
         </Card>
 
